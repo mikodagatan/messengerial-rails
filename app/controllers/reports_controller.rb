565c0fffa8a1885179102a99bab6b7e45101e3d5
Miguel Alberto U. Dagatan <miguel.dagatan@gmail.com>
2017-12-18 15:43:18 +0800

    try

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,7 +3,11 @@ class ReportsController < ApplicationController
   helper_method :by_city_town, :by_city_town_complete, :by_city_town_with_issues, :by_city_town_open, :by_city_town_scheduled
 
   def location_report
-    @locations = Address.pluck(:city_town).uniq
+    @start_date = params[:start_date] || Time.zone.today - 10.years
+    @start_date = Date.new(params[:months]["start_date(1i)"].to_i,params[:months]["start_date(2i)"].to_i, params[:months]["start_date(3i)"].to_i) if params[:months].present?
+    @end_date = params[:end_date] || Time.zone.today + 10.years
+    @end_date = Date.new(params[:months]["end_date(1i)"].to_i,params[:months]["end_date(2i)"].to_i,params[:months]["end_date(3i)"].to_i) if params[:months].present?
+    @locations = Address.where("addresses.created_at between ? and ?", @start_date, @end_date).pluck(:city_town).uniq
   end
 
   def by_city_town(city_town)
